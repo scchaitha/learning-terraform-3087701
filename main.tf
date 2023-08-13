71817afee03db562735f162d4b91f73fdd8fc80f
Shrileckha Chaithanya <cshrileckha@gmail.com>
2023-08-13 14:57:36 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "app_ami" {
 }
 
 
-module "vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
   name = "dev"
@@ -50,7 +50,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog_new"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
   
   ingress_rules        = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
