db31f40638735caa9d4aa8cb096c4585804d3fdd
Shrileckha Chaithanya <cshrileckha@gmail.com>
2023-08-13 14:23:05 -0700

    create dev vpc

--- main.tf
+++ main.tf
@@ -15,8 +15,20 @@ data "aws_ami" "app_ami" {
 }
 
 
-data "aws_vpc" "default" {
-  default = true
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 
@@ -37,7 +49,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets[0]
   
   ingress_rules        = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -47,6 +59,7 @@ module "blog_sg" {
   
 }
 
+# below sg and sg rules not needed and it has been repalced by sg module
 resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http and https in, Allow everything out"
