5259a173e2b6af0cd52b463ea0fb0831f2ddc3d3
Shrileckha Chaithanya <cshrileckha@gmail.com>
2023-08-12 17:09:32 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,6 +14,13 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -24,5 +31,41 @@ resource "aws_instance" "blog" {
   }
 }
 
+
+resource "aws_security_group" "blog" {
+  name = "blog"
+  description = "Allow http and https in, Allow everything out"
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "awes_security_group_rule" "blog_http_in" {
+  type       = "ingress"
+  from_port  = 80
+  to_port    = 80
+  protocol   = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "awes_security_group_rule" "blog_https_in" {
+  type       = "ingress"
+  from_port  = 443
+  to_port    = 443
+  protocol   = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
+
+resource "awes_security_group_rule" "blog_everything_out" {
+  type       = "engress"
+  from_port  = 0
+  to_port    = 0
+  protocol   = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+
+  security_group_id = aws_security_group.blog.id
+}
 
 
