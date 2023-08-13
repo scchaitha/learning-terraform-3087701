cfba3edfafa4057a508e5d70805bb324e26fde92
Shrileckha Chaithanya <cshrileckha@gmail.com>
2023-08-13 15:26:16 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog_new"
 
-  vpc_id = "vpc-07cb80bcdbe36d5e3"
+  vpc_id = module.vpc_blog.public_subnets[0]
   
   ingress_rules        = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
