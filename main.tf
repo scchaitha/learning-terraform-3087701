6c75a5c4d3374c8a30127cb34f6ef2fc462d7057
Shrileckha Chaithanya <cshrileckha@gmail.com>
2023-08-13 15:24:34 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ module "blog_vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   #public_subnets  = ["172.31.32.0/20", "172.31.16.0/20", "172.31.0.0/20"]
 
-  #enable_nat_gateway = true
+  enable_nat_gateway = true
 
   tags = {
     Terraform = "true"
